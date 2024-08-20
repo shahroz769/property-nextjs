7cfad337861f4e2aa73e66a3a9081fa02067af9f
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2024-08-21 01:17:22 +0500

    next-auth, sign-in, sign-out, protected routes

---
+++ utils/authOptions.js
@@ -0,0 +1,50 @@
+import GoogleProvider from "next-auth/providers/google";
+import connectDB from "@/config/database";
+import User from "@/models/User";
+
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code",
+                },
+            },
+        }),
+    ],
+    callbacks: {
+        // Invoked on successful sign in
+        async signIn({ profile }) {
+            //Connnect to database
+            await connectDB();
+            //Check if user exists
+            const userExists = await User.findOne({ email: profile.email });
+            //Create user if not exists
+            if (!userExists) {
+                //Truncate username if too long
+                const username = profile.name.slice(0, 20);
+                //Create user
+                await User.create({
+                    email: profile.email,
+                    username: username,
+                    image: profile.picture,
+                });
+            }
+            //Return true to allow sign in
+            return true;
+        },
+        //Session callback function that modifies the session object
+        async session({ session }) {
+            //Get the user from darabase
+            const user = await User.findOne({ email: session.user.email });
+            //Assign user id from the session
+            session.user.id = user._id.toString();
+            //Return session
+            return session;
+        },
+    },
+};
